7bc4f289627ab92b818a1e08a76c8a765facc6a0
proximalv <cinnamon25@inbox.lv>
2023-06-01 00:42:04 +0300

    izpildits

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-# python3
+#python3
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+    time = [0]*n
+    #inicializejam laika sarakstu ar tik daudz nullem cik thread count
+
+    for i in range(m): # iterējam i+1 lidz vienads ar job count
+        thread = time.index(min(time)) # paņemam vismazāko laika elementu no laika saraksta
+        counter = time[thread] # inicializejam start vertibu nemot laika saraksta atrasto mazako elementu
+        time[thread] += int(data[i]) # pievienojam pie veca laika saraksta mazaka elementa
+        output.append((thread, counter))
+        # vienkarsi pievienojam tuple output sarakstam izvadei
 
     return output
+    # izvadam output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
+    # n,m = map(int, input().split())
+    text = input().split()
+    # n = thread count
+    # m = job count
+    n=text[0]
+    m=text[1]
+    # data = list(map(int, input().split()))
+    data = input().split()
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    result = parallel_processing(int(n),int(m),data)
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
+    for thread, counter in result:
+        print(thread, counter)
 
 
 
